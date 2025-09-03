c31e4e22493366ad7864c2f71dabadc6334e6861
ManiS001 <mkingdom209@gmail.com>
2025-09-03 21:54:00 +0530

    lint Error fixed

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,34 @@
 import { Application, Assets, Sprite } from "pixi.js";
-import {  Spine } from "@esotericsoftware/spine-pixi-v8";
-import {LoadGameAssets, LoadPreloadAssets} from "./loadAssets.ts"
+import { Spine } from "@esotericsoftware/spine-pixi-v8";
+import { LoadGameAssets, LoadPreloadAssets } from "./loadAssets.ts";
 (async () => {
-// exports={};
+  // exports={};
 
-  
   // Create a new application
   const app = new Application();
 
   // Initialize the application
   await app.init({ background: "#1099bb", resizeTo: window });
-(globalThis as any).__PIXI_APP__ = app;
+  // (globalThis).__PIXI_APP__ = app;
   // Append the application canvas to the document body
   document.getElementById("pixi-container")!.appendChild(app.canvas);
 
-  await LoadPreloadAssets()
-  await LoadGameAssets()
+  await LoadPreloadAssets();
+  await LoadGameAssets();
 
-  
- const MainBg_anim = Spine.from({ skeleton:"BaseGame_BG_json", atlas:"BaseGame_BG_atlas", scale:0.5})
-MainBg_anim.x = app.screen.width/2;
-MainBg_anim.y = app.screen.height/2;
- MainBg_anim.state.setAnimation(0, "animation", true);
- app.stage.addChild(MainBg_anim);
-  
+  const MainBg_anim = Spine.from({
+    skeleton: "BaseGame_BG_json",
+    atlas: "BaseGame_BG_atlas",
+    scale: 0.5,
+  });
+  MainBg_anim.x = app.screen.width / 2;
+  MainBg_anim.y = app.screen.height / 2;
+  MainBg_anim.state.setAnimation(0, "animation", true);
+  app.stage.addChild(MainBg_anim);
 
-   const testImage = new Sprite(Assets.get("logo"));
-    testImage.x = app.screen.width/2;
-    testImage.y = app.screen.height/10;
-    testImage.anchor.set(0.5);
+  const testImage = new Sprite(Assets.get("logo"));
+  testImage.x = app.screen.width / 2;
+  testImage.y = app.screen.height / 10;
+  testImage.anchor.set(0.5);
   app.stage.addChild(testImage);
 })();
