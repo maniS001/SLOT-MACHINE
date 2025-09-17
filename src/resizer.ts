d5a599cb8263402a43e66dbef200ae620c5f7339
ManiS001 <mkingdom209@gmail.com>
2025-09-17 23:02:16 +0530

    portrait enabled 2

--- src/resizer.ts
+++ src/resizer.ts
@@ -7,9 +7,9 @@ export function resizeApp(app: PIXI.Application) {
     const new_height = window.innerHeight / AppDimension.height;
     const scale = Math.min(new_width, new_height);
     if (window.matchMedia("(orientation:portrait)").matches) {
-      app.renderer.canvas.style.scale = scale * 1.5 + "";
-    }else{
-      app.renderer.canvas.style.scale = scale + ""; 
+      app.renderer.canvas.style.scale = scale * 1.7 + "";
+    } else {
+      app.renderer.canvas.style.scale = scale + "";
     }
   })();
   // window.onresize()
